77f05f60edd4a7d645cb93416b968798d5bf0f1d
redavillm <leonofforc@yandex.ru>
2023-05-09 17:23:56 +0300

    отладка асинхронности, рефакторинг

--- bot.js
+++ bot.js
@@ -1,34 +1,11 @@
 require("dotenv").config();
-const fetch = require("node-fetch");
-const axios = require("axios");
 
 const TelegramBot = require("node-telegram-bot-api");
+const { getNewsBySubject } = require("./lib/news");
 
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
-
-// const getData = async (them) => {
-//     return await fetch(`https://newsapi.org/v2/everything?q=${them}&apiKey=20f1dd6d7cdb481794e6858275346afb`)
-//     .then(res => res.json())
-//     .then(res => res.articles);
-// };
-
-
-const translation = async (text) => {
-    try {
-        return await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=en&tl=ru&q=${text}`)
-            .then(res => res.json())
-            .then(res => {
-                console.log(res[0][0][0])
-            })
-    } catch (error) {
-        console.log('Error while translation', error);
-    }
-}
-
-// translation('hello world')
-console.log(translation('hello world'))
 
 bot.onText(/\/info/, async (msg) => {
     const chatId = msg.chat.id;
@@ -42,13 +19,9 @@ bot.onText(/\/news (.+)/, async (msg, match) => {
     try {
         const chatId = msg.chat.id;
         const them = match[1].split(" ");
-        await fetch(`https://newsapi.org/v2/everything?q=${them}&apiKey=20f1dd6d7cdb481794e6858275346afb`)
-            .then(res => res.json())
-            .then(res => {
-                res.articles.slice(0, 2).map(el => {
-                    bot.sendMessage(chatId, `${translation(el.title)}\n\n${translation(el.description)}\n\n${el.url}`)
-                })
-            });
+        const texts = await getNewsBySubject(them);
+        console.log(texts)
+        bot.sendMessage(chatId, texts[0]);
 
     }
     catch (error) {
